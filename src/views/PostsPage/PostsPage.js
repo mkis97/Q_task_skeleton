1e870e4ead2bc0f94b10991745603f392bc2eb38
mkis <mkis@LP0273.local>
2022-07-14 11:30:31 +0200

    feat: query string params getter and setter

--- src/views/PostsPage/PostsPage.js
+++ src/views/PostsPage/PostsPage.js
@@ -8,6 +8,7 @@ import { api } from "@/core/api";
 import { mapActions, mapState } from "vuex";
 import { STORE } from "@/core/types/store";
 import { LoaderIndicator } from "@/shared/components/LoaderIndicator";
+import { router } from "@/core/router";
 
 export default {
   name: "PostsPage",
@@ -51,6 +52,14 @@ export default {
 
   async created() {
     try {
+      this.localParams = {
+        q: this.$route.query.q || "",
+        userId: this.$route.query.userId || null,
+        _page: Number(this.$route.query._page) || 1,
+        _limit: Number(this.$route.query._limit) || 20,
+        _sort: this.$route.query._sort || "id",
+        _order: this.$route.query._order || "asc",
+      };
       await this.getPosts();
       await this.getUsers();
       this.isHydrated = true;
@@ -94,15 +103,18 @@ export default {
       deep: true,
       handler() {
         this.debouncedGetPosts();
+        router.replace({ query: this.localParams }).catch(() => {});
       },
     },
 
     "localParams.q"() {
       this.localParams._page = 1;
+      router.replace({ query: this.localParams }).catch(() => {});
     },
 
     "localParams.userId"() {
       this.localParams._page = 1;
+      router.replace({ query: this.localParams }).catch(() => {});
     },
   },
 };
